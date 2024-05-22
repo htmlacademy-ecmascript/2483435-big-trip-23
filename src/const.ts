a022cfcec502d6c6dc7ab9da6b9f75a3f1036a3b
Богдан Троценко <bvtrots@gmail.com>
2024-05-22 10:14:46 +0300

    Работаю над перерисовкой страницы

--- src/const.ts
+++ src/const.ts
@@ -2,8 +2,12 @@ const POINTS_TYPES = ['taxi', 'bus', 'train', 'ship', 'drive', 'flight', 'check-
 
 const SORT_TYPES = ['day', 'event', 'time', 'price', 'offers'] as const;
 const FILTER_TYPES = ['everything', 'future', 'present', 'past'] as const;
+const USER_ACTION = ['updateWaypoint', 'addWaypoint', 'deleteWaypoint'] as const;
+const UPDATE_TYPE = ['patch', 'minor', 'major'] as const;
 type FilterType = (typeof FILTER_TYPES)[number];
 type SortType = (typeof SORT_TYPES)[number];
+type UserAction = (typeof USER_ACTION)[number];
+type UpdateType = (typeof UPDATE_TYPE)[number];
 
 const WaypointsEmptyMessages = {
   everything: 'Click New Event to create your first point',
@@ -12,4 +16,4 @@ const WaypointsEmptyMessages = {
   past: 'There are no past events now',
 } as const satisfies Record<FilterType, string>;
 
-export { POINTS_TYPES, SORT_TYPES, FilterType, WaypointsEmptyMessages, FILTER_TYPES, SortType };
+export { POINTS_TYPES, SORT_TYPES, FILTER_TYPES, WaypointsEmptyMessages, SortType, FilterType, UserAction, UpdateType };
